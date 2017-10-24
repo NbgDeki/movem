99bbf17cd93d323a380e460935d9836a4e3ef267
Dejan <33055364+NbgDeki@users.noreply.github.com>
2017-10-24 13:20:42 +0200

    dodato sve

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+$(function () {
+	
+/********************************************************
+
+                     OWL CAROUSEL
+
+********************************************************/
+
+$(".slider-wrapper").owlCarousel({
+items: 1,
+autoplay:true,
+smartSpeed:700,
+loop: true,
+autoplayHoverPause: true,
+dots: false
+});
+
+$(".novi-proizvodi-slider").owlCarousel({
+items: 5,
+autoplay:true,
+smartSpeed:700,
+loop: true,
+autoplayHoverPause: true,
+dots: false,
+responsive : {
+// breakpoint from 0 up
+0 : {
+items: 1
+},
+// breakpoint from 480 up
+480 : {
+items: 1
+},
+// breakpoint from 768 up
+768 : {
+items: 3
+},
+992 :{
+items: 3
+},
+1199 :{
+items: 5	
+}
+}
+});
+
+});
